fa6430657348c683ee12ca300bece91663e72ba9
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2023-11-01 10:38:03 -0500

    Change cache name and use different builders for each version

--- flake.nix
+++ flake.nix
@@ -2,26 +2,39 @@
   description = "TODO";
 
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
     flake-utils.inputs.systems.follows = "systems";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs-23_05.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
   };
 
-  outputs = { self, flake-utils, nixpkgs, ... }:
+  outputs = { self, flake-utils, nixpkgs-23_05, nixpkgs-unstable, ... }:
     let
       mkPackageName = version: "terraform-${builtins.concatStringsSep "_" (builtins.splitVersion version)}";
     in
     flake-utils.lib.eachDefaultSystem
       (system:
         let
-          pkgs = nixpkgs.legacyPackages.${system};
+          pkgs-23_05 = nixpkgs-23_05.legacyPackages.${system};
+          pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
           versions = builtins.fromJSON (builtins.readFile ./versions.json);
-          # https://github.com/NixOS/nixpkgs/blob/nixpkgs-unstable/pkgs/applications/networking/cluster/terraform/default.nix
-          mkTerraform = { version, hash, vendorHash }: pkgs.mkTerraform {
-            inherit version hash vendorHash;
-            patches = [ "${nixpkgs}/pkgs/applications/networking/cluster/terraform/provider-path-0_15.patch" ];
-          };
+          mkTerraform = { version, hash, vendorHash }:
+            # https://www.hashicorp.com/blog/hashicorp-adopts-business-source-license
+            if builtins.compareVersions version "1.6.0" >= 0
+            then
+            # https://github.com/NixOS/nixpkgs/blob/nixpkgs-unstable/pkgs/applications/networking/cluster/terraform/default.nix
+              pkgs-unstable.mkTerraform
+                {
+                  inherit version hash vendorHash;
+                  patches = [ "${nixpkgs-unstable}/pkgs/applications/networking/cluster/terraform/provider-path-0_15.patch" ];
+                }
+            else
+            # https://github.com/NixOS/nixpkgs/blob/nixos-23.05/pkgs/applications/networking/cluster/terraform/default.nix
+              pkgs-23_05.mkTerraform {
+                inherit version hash vendorHash;
+                patches = [ "${nixpkgs-23_05}/pkgs/applications/networking/cluster/terraform/provider-path-0_15.patch" ];
+              };
         in
         {
           # https://github.com/NixOS/nix/issues/7165
@@ -36,10 +49,6 @@
                 };
               })
               (builtins.attrNames versions));
-          #   templates.default = {
-          #     description = "TODO";
-          #     path = ./templates/default;
-          #   };
         }) // {
       lib.packageFromVersion = { system, version }: self.packages.${system}.${mkPackageName version};
     };
