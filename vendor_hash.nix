d8924f6dd1a41bf1d3e4d40ea2030769fafa2b0e
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2023-11-08 02:38:57 -0500

    Use existing hash and vendorHash

--- vendor_hash.nix
+++ vendor_hash.nix
@@ -2,5 +2,5 @@
 ((builtins.getFlake (toString ./.)).lib.buildTerraform {
   inherit version hash;
   system = builtins.currentSystem;
-  vendorHash = "";
-}).go-modules.overrideAttrs (_: { vendorSha256 = sha256; })
+  vendorHash = sha256;
+}).goModules
