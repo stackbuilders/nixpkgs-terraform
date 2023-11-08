6037d00e29a4bb358b5ee70fda1d9a6eb5c8e4b0
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2023-11-08 01:45:39 -0500

    Expose buildTerraform / Update update_versions script

--- vendor_hash.nix
+++ vendor_hash.nix
@@ -1,5 +1,6 @@
-{ version }: { sha256 }:
-((builtins.getFlake (toString ./.)).lib.packageFromVersion {
-  inherit version;
+{ version, hash }: { sha256 }:
+((builtins.getFlake (toString ./.)).lib.buildTerraform {
+  inherit version hash;
   system = builtins.currentSystem;
+  vendorHash = "";
 }).go-modules.overrideAttrs (_: { vendorSha256 = sha256; })
