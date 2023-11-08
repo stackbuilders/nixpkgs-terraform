e5b387b55d963053a3a7e8af3ead71c398d0162c
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2023-11-08 10:28:00 -0500

    Fix vendor_hash.nix

--- vendor_hash.nix
+++ vendor_hash.nix
@@ -1,6 +1,9 @@
 { version, hash }: { sha256 }:
-((builtins.getFlake (toString ./.)).lib.buildTerraform {
-  inherit version hash;
-  system = builtins.currentSystem;
-  vendorHash = sha256;
-}).goModules
+let
+  package = ((builtins.getFlake (toString ./.)).lib.buildTerraform {
+    inherit version hash;
+    system = builtins.currentSystem;
+    vendorHash = sha256;
+  });
+in
+  package.goModules or package.go-modules
