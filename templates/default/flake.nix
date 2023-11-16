3e556295d317d545be31c0b4fa2385e1d629b0e3
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2023-11-15 20:04:03 -0500

    Update README file and add LICENSE (#4)
    * Update README file and add LICENSE
    
    * Add headline and fix broken link
    
    * Update Install and Usage section based on default template
    
    * Fix template output
    
    * Update intro
    
    * Remove flake.lock from default template

---
+++ templates/default/flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs-terraform.url = "github:stackbuilders/nixpkgs-terraform";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  nixConfig = {
+    extra-substituters = "https://nixpkgs-terraform.cachix.org";
+    extra-trusted-public-keys = "nixpkgs-terraform.cachix.org-1:8Sit092rIdAVENA3ZVeH9hzSiqI/jng6JiCrQ1Dmusw=";
+  };
+
+  outputs = { self, flake-utils, nixpkgs-terraform, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        terraform = nixpkgs-terraform.packages.${system}."1.6.3";
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ terraform ];
+        };
+      });
+}
