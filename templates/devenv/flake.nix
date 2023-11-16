86c7bf5ceabebea42f79dbd00454d47a4776a458
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2023-11-15 21:22:10 -0500

    Test devenv template

---
+++ templates/devenv/flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    devenv.url = "github:cachix/devenv";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs-terraform.url = "github:stackbuilders/nixpkgs-terraform";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  nixConfig = {
+    extra-substituters = "https://nixpkgs-terraform.cachix.org";
+    extra-trusted-public-keys = "nixpkgs-terraform.cachix.org-1:8Sit092rIdAVENA3ZVeH9hzSiqI/jng6JiCrQ1Dmusw=";
+  };
+
+  outputs = inputs@{ self, devenv, flake-utils, nixpkgs-terraform, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        terraform = nixpkgs-terraform.packages.${system}."1.6.3";
+      in
+      {
+        devShells.default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            ({ pkgs, config, ... }: {
+              languages.python.enable = true;
+              languages.python.package = packages.python;
+            })
+          ];
+        };
+      });
+}
