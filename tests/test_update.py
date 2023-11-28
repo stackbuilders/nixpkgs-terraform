75f545ac32e6eb4de696a97d61d88565ab70b361
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2023-11-28 09:33:23 -0500

    WIP

--- tests/test_update.py
+++ tests/test_update.py
@@ -1,6 +1,10 @@
+from unittest.mock import patch
 import unittest
 import update
 
 class UpdateTestCase(unittest.TestCase):
-    def test_foo(self):
-        self.assertTrue(False)
+    @unittest.
+    def test_is_stable(self):
+        with unittest.mock.patch("github.GitRelease.GitRelease") as mock:
+            moc
+            self.assertTrue(update.is_stable(mock))
