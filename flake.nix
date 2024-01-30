869f7d3cc62ad9c7006995635390aad037d18db1
Óscar Izquierdo Valentín <25722135+oscar-izval@users.noreply.github.com>
2024-01-30 21:29:04 +0100

    feat: Drop support for Terraform 1.5 versions (#26)
    BREAKING CHANGE: Terraform 1.5 versions would no longer be provided by this flake.

--- flake.nix
+++ flake.nix
@@ -2,30 +2,28 @@
   inputs = {
     flake-utils.inputs.systems.follows = "systems";
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     systems.url = "github:nix-systems/default";
   };
 
-  outputs = { self, flake-utils, nixpkgs-unstable, nixpkgs, ... }:
+  outputs = { self, flake-utils, nixpkgs, ... }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
           versions = builtins.fromJSON (builtins.readFile ./versions.json);
         in
         {
           devShells.default = pkgs.mkShell {
             buildInputs = [
-              pkgs-unstable.black
-              (pkgs-unstable.python3.withPackages
+              pkgs.black
+              (pkgs.python3.withPackages
                 (ps: [
                   ps.pygithub
                   ps.semver
                 ])
               )
-              pkgs-unstable.nix-prefetch
+              pkgs.nix-prefetch
               pkgs.nodejs
               pkgs.rubyPackages.dotenv
             ];
@@ -41,23 +39,13 @@
       lib.buildTerraform = { system, version, hash, vendorHash }:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
         in
-        # https://www.hashicorp.com/blog/hashicorp-adopts-business-source-license
-        if builtins.compareVersions version "1.6.0" >= 0
-        then
-        # https://github.com/NixOS/nixpkgs/blob/nixpkgs-unstable/pkgs/applications/networking/cluster/terraform/default.nix
-          pkgs-unstable.mkTerraform
+          # https://github.com/NixOS/nixpkgs/blob/nixpkgs-unstable/pkgs/applications/networking/cluster/terraform/default.nix
+          pkgs.mkTerraform
             {
               inherit version hash vendorHash;
-              patches = [ "${nixpkgs-unstable}/pkgs/applications/networking/cluster/terraform/provider-path-0_15.patch" ];
-            }
-        else
-        # https://github.com/NixOS/nixpkgs/blob/nixos-23.05/pkgs/applications/networking/cluster/terraform/default.nix
-          pkgs.mkTerraform {
-            inherit version hash vendorHash;
-            patches = [ "${nixpkgs}/pkgs/applications/networking/cluster/terraform/provider-path-0_15.patch" ];
-          };
+              patches = [ "${nixpkgs}/pkgs/applications/networking/cluster/terraform/provider-path-0_15.patch" ];
+            };
       templates = {
         default = {
           description = "Simple nix-shell with Terraform installed via nixpkgs-terraform";
