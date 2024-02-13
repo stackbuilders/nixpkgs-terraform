a6d136ef5440227c939f4120ab80ccd06612c413
Nikita Pedorich <pedorich.n@gmail.com>
2024-02-13 19:37:26 +0900

    Use flake-parts

---
+++ packages.nix
@@ -0,0 +1,9 @@
+{ custom-lib, pkgs, pkgs-unstable }:
+let
+  versions = builtins.fromJSON (builtins.readFile ./versions.json);
+in
+builtins.mapAttrs
+  (version: { hash, vendorHash }: custom-lib.buildTerraform {
+    inherit pkgs pkgs-unstable version hash vendorHash;
+  })
+  versions
