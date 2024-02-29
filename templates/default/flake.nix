d7ceb00cd56587a7bb2e2af5a45f6c7e89bdaae5
Oscar Izquierdo <25722135+oscar-izval@users.noreply.github.com>
2024-02-29 14:27:14 +0100

    Bump terraform version for templates

--- templates/default/flake.nix
+++ templates/default/flake.nix
@@ -14,7 +14,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        terraform = nixpkgs-terraform.packages.${system}."1.7.3";
+        terraform = nixpkgs-terraform.packages.${system}."1.7.4";
       in
       {
         devShells.default = pkgs.mkShell {
--- templates/terranix/flake.nix
+++ templates/terranix/flake.nix
@@ -15,7 +15,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        terraform = nixpkgs-terraform.packages.${system}."1.7.3";
+        terraform = nixpkgs-terraform.packages.${system}."1.7.4";
       in
       {
         defaultPackage = terranix.lib.terranixConfiguration {
