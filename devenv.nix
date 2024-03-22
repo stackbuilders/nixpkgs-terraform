03544689dd0dc66865e237fd8487d55ce6d071b0
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-03-21 21:05:27 -0500

    Add hook to check Python format

--- devenv.nix
+++ devenv.nix
@@ -5,6 +5,7 @@
     pkgs.semantic-release
   ];
 
+  pre-commit.hooks.black.enable = true;
   pre-commit.hooks.nixpkgs-fmt.enable = true;
 
   # See full reference at https://devenv.sh/reference/options/
