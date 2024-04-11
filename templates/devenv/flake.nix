4f3ecbf4e0f40a9de7ccf968a9dab2a07a3f2eed
Oscar Izquierdo <25722135+oscar-izval@users.noreply.github.com>
2024-04-11 23:59:00 +0200

    Remove debugging lines

--- templates/devenv/flake.nix
+++ templates/devenv/flake.nix
@@ -6,12 +6,12 @@
     systems.url = "github:nix-systems/default";
   };
 
-  # nixConfig = {
-  #   extra-substituters = "https://nixpkgs-terraform.cachix.org";
-  #   extra-trusted-public-keys = "nixpkgs-terraform.cachix.org-1:8Sit092rIdAVENA3ZVeH9hzSiqI/jng6JiCrQ1Dmusw=";
-  # };
+  nixConfig = {
+    extra-substituters = "https://nixpkgs-terraform.cachix.org";
+    extra-trusted-public-keys = "nixpkgs-terraform.cachix.org-1:8Sit092rIdAVENA3ZVeH9hzSiqI/jng6JiCrQ1Dmusw=";
+  };
 
-  outputs = inputs@{ self, devenv, nixpkgs-terraform, nixpkgs, systems }:
+  outputs = inputs@{ self, devenv, nixpkgs, systems }:
     let
       forEachSystem = nixpkgs.lib.genAttrs (import systems);
     in
@@ -26,8 +26,6 @@
               inherit inputs pkgs;
               modules = [
                 ({ pkgs, config, ... }: {
-                  cachix.pull = [ "nixpkgs-terraform" "devenv" ];
-
                   languages.terraform.enable = true;
                   languages.terraform.version = "1.7.4";
                 })
