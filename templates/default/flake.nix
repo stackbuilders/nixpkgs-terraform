a083c46ac8fcb39c14c25e68299bce6755e69bd3
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-04-22 22:21:18 -0500

    Update templates

--- templates/default/flake.nix
+++ templates/default/flake.nix
@@ -19,7 +19,7 @@
         (system:
           let
             pkgs = nixpkgs.legacyPackages.${system};
-            terraform = nixpkgs-terraform.packages.${system}."1.7.4";
+            terraform = nixpkgs-terraform.packages.${system}."1.8.1";
           in
           {
             default = pkgs.mkShell {
