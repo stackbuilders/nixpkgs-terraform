427d212d16105dc736219921642712e58fb1d4c3
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-06-27 12:58:55 -0500

    Merge devenv files

--- devenv.nix
+++ devenv.nix
@@ -4,9 +4,12 @@
   dotenv.enable = true;
 
   packages = [
+    pkgs.cobra-cli
     pkgs.nix-prefetch
     pkgs.semantic-release
   ];
+
+  languages.go.enable = true;
 
   pre-commit.hooks.nixpkgs-fmt.enable = true;
 }
