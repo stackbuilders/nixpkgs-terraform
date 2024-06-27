3931d4644298fe7a547d964ea4dddcd0609e7668
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-06-27 09:03:33 -0500

    Update descriptions

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -8,13 +8,8 @@ import (
 
 var rootCmd = &cobra.Command{
 	Use:   "cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A set of tools for maintainers",
+	Long:  "A set of tools for maintainers",
 }
 
 func Execute() {
