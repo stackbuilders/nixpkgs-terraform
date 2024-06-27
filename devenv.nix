116b939b2aeeb7e92b30f283dcb5634cab00e328
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-06-27 10:09:45 -0500

    Update devenv files

--- devenv.nix
+++ devenv.nix
@@ -4,12 +4,9 @@
   dotenv.enable = true;
 
   packages = [
-    pkgs.cobra-cli
     pkgs.nix-prefetch
     pkgs.semantic-release
   ];
-
-  languages.go.enable = true;
 
   pre-commit.hooks.nixpkgs-fmt.enable = true;
 }
