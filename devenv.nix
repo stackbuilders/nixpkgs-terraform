309ecc05032edaf0ec21c57be5afdc1c9e5b28af
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-06-27 09:56:19 -0500

    Run tool from root directory

--- devenv.nix
+++ devenv.nix
@@ -1,9 +1,15 @@
 { pkgs, ... }:
 
 {
+  dotenv.enable = true;
+
   packages = [
+    pkgs.cobra-cli
+    pkgs.nix-prefetch
     pkgs.semantic-release
   ];
+
+  languages.go.enable = true;
 
   pre-commit.hooks.nixpkgs-fmt.enable = true;
 }
