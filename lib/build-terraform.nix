88dc3a54d3b20877dbc27539b41a089a52af41ef
Oscar Izquierdo <25722135+oscar-izval@users.noreply.github.com>
2024-07-05 11:13:49 +0200

    Add new patch for >1.9

--- lib/build-terraform.nix
+++ lib/build-terraform.nix
@@ -1,6 +1,14 @@
 { pkgs, pkgs-unstable, version, hash, vendorHash, silenceWarnings ? false }:
 # https://www.hashicorp.com/blog/hashicorp-adopts-business-source-license
-if builtins.compareVersions version "1.6.0" >= 0
+if builtins.compareVersions version "1.9.0" >= 0
+then
+# https://github.com/NixOS/nixpkgs/blob/nixpkgs-unstable/pkgs/applications/networking/cluster/terraform/default.nix
+  (pkgs.lib.warnIf (! silenceWarnings) ("allowUnfree is enabled to build version " + version) pkgs-unstable.mkTerraform
+  {
+    inherit version hash vendorHash;
+    patches = [ ../patches/provider-path-1_9.patch ];
+  })
+else if builtins.compareVersions version "1.6.0" >= 0
 then
 # https://github.com/NixOS/nixpkgs/blob/nixpkgs-unstable/pkgs/applications/networking/cluster/terraform/default.nix
   (pkgs.lib.warnIf (! silenceWarnings) ("allowUnfree is enabled to build version " + version) pkgs-unstable.mkTerraform
