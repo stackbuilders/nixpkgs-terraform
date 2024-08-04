69c4ebdb5cc6dd9d35dfbf978d8786083369a002
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-08-04 15:54:24 -0500

    Fixed nixpkgs-1_9 commit

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs-1_0.url = "github:nixos/nixpkgs/41de143fda10e33be0f47eab2bfe08a50f234267";
     nixpkgs-1_6.url = "github:nixos/nixpkgs/d6b3ddd253c578a7ab98f8011e59990f21dc3932";
-    nixpkgs-1_9.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs-1_9.url = "github:nixos/nixpkgs/f5fd8730397b9951d24de58f51a5e9cb327e2a85";
     systems.url = "github:nix-systems/default";
   };
 
