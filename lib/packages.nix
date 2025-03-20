cfa2aa8da2048068c504ae6a9a86ae420b45b663
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2025-03-20 17:25:58 -0500

    Remove code indirection

--- lib/packages.nix
+++ lib/packages.nix
@@ -8,16 +8,6 @@
 ,
 }:
 
-let
-  pkgsByVersion =
-    version:
-    if builtins.compareVersions version "1.9.0" >= 0 then
-      allPkgs."1.9"
-    else if builtins.compareVersions version "1.6.0" >= 0 then
-      allPkgs."1.6"
-    else
-      allPkgs."1.0";
-in
 builtins.mapAttrs
   (
     version:
@@ -29,7 +19,13 @@ builtins.mapAttrs
         vendorHash
         silenceWarnings
         ;
-      pkgs = pkgsByVersion version;
+      pkgs =
+        if builtins.compareVersions version "1.9.0" >= 0 then
+          allPkgs."1.9"
+        else if builtins.compareVersions version "1.6.0" >= 0 then
+          allPkgs."1.6"
+        else
+          allPkgs."1.0";
     }
   )
   releases
