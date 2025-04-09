9fdddb5b09520d4c4a4cd79c4eca9e98ed6d4d82
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2025-04-08 21:04:49 -0500

    feat(chore): Get rid of indirections inside `lib` module (#119)

--- lib/default.nix
+++ lib/default.nix
@@ -1,5 +1,82 @@
-rec {
-  buildTerraform = import ./build.nix;
-  __buildTerraformFor = import ./build-for.nix { inherit buildTerraform; };
-  __mkPackages = import ./packages.nix { inherit __buildTerraformFor; };
+let
+  buildTerraform =
+    { pkgs
+    , version
+    , hash
+    , vendorHash
+    ,
+    }:
+
+    pkgs.lib.warnIf (builtins.compareVersions version "1.6.0" >= 0)
+      ("allowUnfree is enabled to build version " + version)
+      pkgs.mkTerraform
+      {
+        inherit version hash vendorHash;
+        patches =
+          if builtins.compareVersions version "1.9.0" >= 0 then
+            [ ../patches/provider-path-1_9.patch ]
+          else
+            [ ../patches/provider-path-0_15.patch ];
+      };
+
+  __buildTerraformFor =
+    { inputs
+    , system
+    , version
+    , hash
+    , vendorHash
+    ,
+    }:
+
+    let
+      pkgs-1_9 = import inputs.nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      pkgs-1_6 = import inputs.nixpkgs-1_6 {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      pkgs-1_0 = import inputs.nixpkgs-1_0 { inherit system; };
+    in
+    buildTerraform {
+      inherit
+        version
+        hash
+        vendorHash
+        ;
+      pkgs =
+        if builtins.compareVersions version "1.9.0" >= 0 then
+          pkgs-1_9
+        else if builtins.compareVersions version "1.6.0" >= 0 then
+          pkgs-1_6
+        else
+          pkgs-1_0;
+    };
+
+  __mkPackages =
+    { inputs
+    , system
+    , releases
+    ,
+    }:
+
+    builtins.mapAttrs
+      (
+        version:
+        { hash, vendorHash }:
+        __buildTerraformFor {
+          inherit
+            inputs
+            system
+            version
+            hash
+            vendorHash
+            ;
+        }
+      )
+      releases;
+in
+{
+  inherit buildTerraform __buildTerraformFor __mkPackages;
 }
