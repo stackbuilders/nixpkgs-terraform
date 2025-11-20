2e00c015e513efa1981a46df1c06754f097c6655
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2025-11-19 23:48:34 -0500

    Pass a function to overrideAttrs

--- lib/default.nix
+++ lib/default.nix
@@ -2,27 +2,25 @@
 
 {
   mkTerraform =
-    { system ? builtins.currentSystem
-    , version
-    , hash
-    , vendorHash
-    ,
+    {
+      system ? builtins.currentSystem,
+      version,
+      hash,
+      vendorHash,
     }:
 
     let
       pkgs =
         if builtins.compareVersions version "1.9.0" >= 0 then
-          import inputs.nixpkgs
-            {
-              inherit system;
-              config.allowUnfree = true;
-            }
+          import inputs.nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          }
         else if builtins.compareVersions version "1.6.0" >= 0 then
-          import inputs.nixpkgs-24_05
-            {
-              inherit system;
-              config.allowUnfree = true;
-            }
+          import inputs.nixpkgs-24_05 {
+            inherit system;
+            config.allowUnfree = true;
+          }
         else
           inputs.nixpkgs-23_05.legacyPackages.${system};
     in
@@ -45,16 +43,16 @@
       };
 
   mkOpentofu =
-    { system ? builtins.currentSystem
-    , version
-    , hash
-    , vendorHash
-    ,
+    {
+      system ? builtins.currentSystem,
+      version,
+      hash,
+      vendorHash,
     }:
     let
       pkgs = inputs.nixpkgs.legacyPackages.${system};
     in
-    pkgs.opentofu.overrideAttrs {
+    pkgs.opentofu.overrideAttrs (_: {
       inherit version vendorHash;
       src = pkgs.fetchFromGitHub {
         inherit hash;
@@ -62,30 +60,28 @@
         repo = "opentofu";
         tag = "v${version}";
       };
-    };
+    });
 
   mkReleases =
-    { system
-    , releases
-    , namePrefix
-    , mkRelease
-    ,
+    {
+      system,
+      releases,
+      namePrefix,
+      mkRelease,
     }:
-    inputs.nixpkgs.lib.mapAttrs'
-      (
-        version:
-        { hash, vendorHash }:
-        {
-          name = "${namePrefix}-${version}";
-          value = mkRelease {
-            inherit
-              system
-              version
-              hash
-              vendorHash
-              ;
-          };
-        }
-      )
-      releases;
+    inputs.nixpkgs.lib.mapAttrs' (
+      version:
+      { hash, vendorHash }:
+      {
+        name = "${namePrefix}-${version}";
+        value = mkRelease {
+          inherit
+            system
+            version
+            hash
+            vendorHash
+            ;
+        };
+      }
+    ) releases;
 }
