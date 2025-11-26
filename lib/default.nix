eaaff02ee9424907ae6bdfcb7bc091a54e34bf4b
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2025-11-26 10:11:06 -0500

    feat: Add Terraform version 1.14.0 (#163)

--- lib/default.nix
+++ lib/default.nix
@@ -36,7 +36,9 @@
       {
         inherit version hash vendorHash;
         patches =
-          if builtins.compareVersions version "1.9.0" >= 0 then
+          if builtins.compareVersions version "1.14.0" >= 0 then
+            [ ../patches/provider-path-1_14.patch ]
+          else if builtins.compareVersions version "1.9.0" >= 0 then
             [ ../patches/provider-path-1_9.patch ]
           else
             [ ../patches/provider-path-0_15.patch ];
