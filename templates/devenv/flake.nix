0cf567d04532aa2413bee12766086b9d04ef0964
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2026-02-12 11:57:43 -0500

    fix: Fix templates build

--- templates/devenv/flake.nix
+++ templates/devenv/flake.nix
@@ -6,31 +6,42 @@
     systems.url = "github:nix-systems/default";
   };
 
-  nixConfig = {
-    extra-substituters = "https://nixpkgs-terraform.cachix.org";
-    extra-trusted-public-keys = "nixpkgs-terraform.cachix.org-1:8Sit092rIdAVENA3ZVeH9hzSiqI/jng6JiCrQ1Dmusw=";
-  };
+  # nixConfig = {
+  #   extra-substituters = "https://nixpkgs-terraform.cachix.org";
+  #   extra-trusted-public-keys = "nixpkgs-terraform.cachix.org-1:8Sit092rIdAVENA3ZVeH9hzSiqI/jng6JiCrQ1Dmusw=";
+  # };
 
-  outputs = inputs@{ self, devenv, nixpkgs-terraform, nixpkgs, systems }:
+  outputs =
+    inputs@{ self
+    , devenv
+    , nixpkgs-terraform
+    , nixpkgs
+    , systems
+    ,
+    }:
     let
       forEachSystem = nixpkgs.lib.genAttrs (import systems);
     in
     {
-      devShells = forEachSystem
-        (system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-          in
-          {
-            default = devenv.lib.mkShell {
-              inherit inputs pkgs;
-              modules = [
-                ({ pkgs, config, ... }: {
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [
+              (
+                { pkgs, config, ... }:
+                {
                   languages.terraform.enable = true;
                   languages.terraform.version = "1.14";
-                })
-              ];
-            };
-          });
+                }
+              )
+            ];
+          };
+        }
+      );
     };
 }
